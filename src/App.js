6c97750ad608db39e2ed7ba0d2f8cc9cbb1a32c8
Darío Rumbo <dario.rumbo@gmail.com>
2022-01-07 22:21:35 -0300

    Mejoras

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/Navigation';
+
+import { tareas } from './bd.json'
+import Tarea from './components/Tarea';
+import FormTareas from './components/FormTareas';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+        title: 'Aplicación de Tareas',
+        tareas: tareas,
+    }
+  }
+  handleAddTarea = (tarea) => {
+    this.setState({
+      tareas: [...this.state.tareas, tarea]
+    })
+  }
+  handleRemoveTarea = (index) => {
+    if (window.confirm('¿Estás seguro de eliminar la tarea?'))
+      this.setState({
+        tareas: this.state.tareas.filter((tarea, i) => {
+          return i !== index
+        })
+      })
+  }
+  render() {
+    const tareas = this.state.tareas.map((tarea, i) => {
+      return (
+        <Tarea tarea={tarea} key={i} onRemoveTarea={() => this.handleRemoveTarea(i)} />
+      )
+    })
+    return (
+      <div className="App">
+        <Navigation title={this.state.title} tareas={tareas} />
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col-md-3">
+              <FormTareas onAddTarea={this.handleAddTarea} />
+              <p>Tírate a un pozo, chango!</p>
+            </div>
+            <div className="col-md-9">
+              <div className="row">
+                { tareas }
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
